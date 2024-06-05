90e62633f2c6b6a2f4c576761b127e364111788b
Dale Ren <daleren.y@gmail.com>
2024-06-05 15:48:39 -0400

    surprisingly difficult; fuggin bit patterns man

--- cses/introductory-problems/grayCode.cpp
+++ cses/introductory-problems/grayCode.cpp
@@ -1,25 +1,38 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
-void generate(unsigned int totalBits, string& currCode);
+void generate(unsigned int n, vector<string>& result);
 
 int main() {
   unsigned int n;
   cin >> n;
+  vector<string> result (1, "");
 
-  string code = "";
-  generate(n, code);
+  generate(n, result);
+  for(unsigned int i = 0; i < result.size(); i++) {
+    cout << result[i] << endl;
+  }
 }
 
-void generate(unsigned int totalBits, string& currCode) {
-  if (currCode.size() == totalBits) {
-    cout << currCode << endl;
+void generate(unsigned int n, vector<string>& result) {
+  if (result[0].size() == n) {
     return;
   }
-  for(int i = 0; i <= 1; i++) {
-    currCode += to_string(i);
-    generate(totalBits, currCode);
-    currCode.pop_back();
+
+  result.resize(result.size() * 2);
+  for(unsigned int i = 0; i < result.size() / 2; i++) {
+    result[(result.size()-1) - i] = result[i];
   }
+
+  for(unsigned int i = 0; i < result.size(); i++) {
+    if (i < result.size() / 2) {
+      result[i].insert(0, "0");
+    } else {
+      result[i].insert(0, "1");
+    }
+  }
+
+  generate(n, result);
 }
